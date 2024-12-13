df443f05a24f4a39656fae85de5620622d08c3bf
acer NITRO 5 <feruzbekhamrayev2002@gmail.com>
2024-12-13 17:57:46 +0500

    Troyan harakati tug'ilgan kunlar uchun boshlandi

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ConnectPage from './dashboard/connect';
+
+function App() {
+  return (
+    <div className="App">
+
+      <ConnectPage />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/dashboard/connect.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ConnectFiunc = () => {
+
+    return (
+
+        <div className="connectContainer">
+
+            Connect ...
+        </div>
+    )
+}
+
+export default ConnectFiunc;
